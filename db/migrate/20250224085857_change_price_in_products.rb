bcd1419c389cfc34c5488b2fa0e536cbda19fef9
Aayush Karki <aayushkarki2001@gmail.com>
2025-02-25 10:03:57 +0900

    done with the seeding

--- db/migrate/20250224085857_change_price_in_products.rb
+++ db/migrate/20250224085857_change_price_in_products.rb
@@ -1,5 +1,9 @@
 class ChangePriceInProducts < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_column :products, :price, :decimal, precision: 10, scale: 2
+  end
+
+  def down
+    change_column :products, :price, :integer # Change this to the previous type
   end
 end
