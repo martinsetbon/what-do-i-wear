555d305c5a5fbed933dd96a02957fd7800a2e608
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-02-27 11:40:47 +0900

    can upload photo

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,2 +1,35 @@
 class OutfitsController < ApplicationController
+
+  def show
+    @outfit = Outfit.find(params[:id])
+  end
+
+  def new
+    @outfit = Outfit.new
+  end
+
+  def create
+    @outfit = Outfit.new(outfit_params)
+    @outfit.user = current_user # Associate the user
+    if @outfit.save
+      redirect_to edit_outfit_path(@outfit), notice: "Outfit created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @outfit = Outfit.find(params[:id])
+    @outfit.user = current_user
+    @product_top = OutfitProduct.new
+    @product_bottom = OutfitProduct.new
+    @product_shoes = OutfitProduct.new
+  end
+
+  private
+
+  # Only allow the photo parameter in the outfit_params
+  def outfit_params
+    params.require(:outfit).permit(:photo)
+  end
 end
--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,13 +1,12 @@
 class Outfit < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
   has_many :outfit_products
   has_many :products, through: :outfit_products
-
-  validates :name, presence: true
 
   # Optional fields
   validates :budget, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
   validates :season, inclusion: { in: %w(summer fall winter spring), message: "%{value} is not a valid season" }, allow_nil: true
   validates :style, inclusion: { in: %w(casual formal), message: "%{value} is not a valid style" }, allow_nil: true
-  validates :gender, inclusion: { in: %w(male female androgynous), message: "%{value} is not a valid gender" }, allow_nil: true
+  validates :gender, inclusion: { in: %w(male female unisex), message: "%{value} is not a valid gender" }, allow_nil: true
 end
