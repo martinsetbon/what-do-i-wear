55a06fbe06f90fbc1fa045f79c69eb1950652c19
martin setbon <setbon.martin@gmail.com>
2025-02-28 10:33:49 +0900

    create the upload photo outfit creation

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,10 +1,12 @@
 class Outfit < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
-  has_many :outfit_products
+  has_many :outfit_products, dependent: :destroy
   has_many :products, through: :outfit_products
 
   # Optional fields
+  validates :name, presence: true
+  validates :photo, presence: { message: "can't be blank. Please upload a photo." }
   validates :budget, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
   validates :season, inclusion: { in: %w(summer fall winter spring), message: "%{value} is not a valid season" }, allow_nil: true
   validates :style, inclusion: { in: %w(casual formal), message: "%{value} is not a valid style" }, allow_nil: true
