64b0d62bbf1204b5d9d7767d6b2832e87fbe0191
martin setbon <setbon.martin@gmail.com>
2025-03-06 10:55:33 +0900

    solid cabl problem

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module WhatDoIWear
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    config.active_job.queue_adapter = :solid_queue
+    config.active_job.queue_adapter = :inline
 
     # Configuration for the application, engines, and railties goes here.
     #
